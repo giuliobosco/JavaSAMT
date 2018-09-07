65edd9224eb106aef8610737195f42b2f4f743db
Giulio Bosco <giuliobva@gmail.com>
2018-09-07 19:46:06 +0200

    create APP

---
+++ src/mybutton/Main.java
@@ -0,0 +1,152 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package mybutton;
+
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * Main Application Class.
+ * Create the frame (window) and add the button.
+ *
+ * @author giuliobosco
+ * @version 04.06.2018
+ */
+public class Main extends JFrame implements MouseListener, MyButtonListener {
+    
+    /**
+     * Button of the window.
+     */
+    private MyButton button;
+    
+    /**
+     * Constructor with window title.
+     * @param title Window Title.
+     */
+    public Main(String title) {
+        super(title);
+        
+        this.setSize(300,200);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.addMouseListener(this);
+        button = new MyButton(20,20,100,80,Color.red,"MyButton");
+        button.addMyButtonListener(this);
+    }
+
+    /**
+     * Mouse Clicked event.
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        button.mouseClicked(e);
+    }
+
+    /**
+     * Mouse Pressed event.
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {
+        button.mousePressed(e);
+    }
+
+    /**
+     * Mouse Released event.
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        button.mouseReleased(e);
+    }
+
+    /**
+     * Mouse Entered event (not implemented).
+     * @param e Mouse Event
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    /**
+     * Mouse exited event (not implemented).
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+
+    /**
+     * Button Clicked event.
+     * @param source Event source.
+     */
+    @Override
+    public void buttonClicked(MyButton source) {
+        System.out.println("button clicked");
+    }
+
+    /**
+     * Button Pressed event.
+     * @param source Event source.
+     */
+    @Override
+    public void buttonPressed(MyButton source) {
+        System.out.println("button pressed");
+    }
+
+    /**
+     * Button released.
+     * @param source Event source.
+     */
+    @Override
+    public void buttonReleased(MyButton source) {
+        System.out.println("button released");
+    }
+
+    /**
+     * Paint the windows view.
+     * @param g Graphics of the window.
+     */
+    @Override
+    public void paint(Graphics g) {
+        button.paint(g); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Application runner.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main("Demo").setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/mybutton/MyButton.java
@@ -0,0 +1,165 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package mybutton;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Graphics;
+
+/**
+ * MyButton.
+ * @author giuliobosco
+ * @version 28.05.2018
+ */
+public class MyButton implements MouseListener{
+    /**
+     * Y Position of the button in the view.
+     */
+    private int y;
+    
+    /**
+     * X Position of the button in the view.
+     */
+    private int x;
+    
+    /**
+     * Width of the button.
+     */
+    private int w = 100;
+    /**
+     * Height of the button.
+     */
+    private int h = 80;
+    
+    /**
+     * Button Color.
+     */
+    private Color color = Color.blue;
+    
+    /**
+     * Label of the button
+     */
+    private String content = "UNNAME";
+    
+    /**
+     * Button listeners.
+     */
+    private List<MyButtonListener> listeners;
+    
+    /**
+     * Constructor with y position, x position, width, height, color, label content.
+     * @param y Y position of the button in the view.
+     * @param x X position of the button in the view.
+     * @param w width of the button.
+     * @param h height of the button.
+     * @param color color of the button.
+     * @param content content of the button.
+     */
+    public MyButton(int y, int x, int w, int h, Color color, String content) {
+        this.y = y;
+        this.x = x;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+        this.content = content;
+        listeners = new ArrayList<>();
+    }
+    
+    /**
+     * Add listener to listeners.
+     * @param listener Listener to add.
+     */
+    public void addMyButtonListener(MyButtonListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Mouse Clicked Event.
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getX() >= this.x && e.getX() <= this.x + this.w && e.getY() >= this.y && e.getY() <= this.y + this.h) {
+            for (MyButtonListener listener : listeners) {
+                listener.buttonClicked(this);
+            }
+        } 
+    }
+
+    /**
+     * Mouse Pressed Event.
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getX() >= this.x && e.getX() <= this.x + this.w && e.getY() >= this.y && e.getY() <= this.y + this.h) {
+            for (MyButtonListener listener : listeners) {
+                listener.buttonPressed(this);
+            }
+        }
+    }
+
+    /**
+     * Mouse Released Event.
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (e.getX() >= this.x && e.getX() <= this.x + this.w && e.getY() >= this.y && e.getY() <= this.y + this.h) {
+            for (MyButtonListener listener : listeners) {
+                listener.buttonReleased(this);
+            }
+        }
+    }
+
+    /**
+     * Mouse Entered Event (not implemented).
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    /**
+     * Mouse Exited Event (not implemented).
+     * @param e Mouse Event.
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+    
+    /**
+     * Paint graphics of the button.
+     * @param g Graphics of the window.
+     */
+    public void paint(Graphics g) {
+        g.setColor(color);
+        g.fillRect(x, y, w, h);
+    }
+}
