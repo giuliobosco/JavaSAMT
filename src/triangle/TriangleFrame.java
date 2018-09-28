dbfeab6c0bef3c1ae6fbfbabab74ece12371d71b
Giulio Bosco <giuliobva@gmail.com>
2018-09-28 11:11:49 +0200

    fix TriangleFrame - border layout

--- src/triangle/TriangleFrame.java
+++ src/triangle/TriangleFrame.java
@@ -29,17 +29,6 @@ public class TriangleFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
