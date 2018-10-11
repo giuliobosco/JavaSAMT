68e99d1b0cd545c1b5e4edadf12c3e84276c4cbd
Giulio Bosco <giuliobva@gmail.com>
2018-10-11 09:41:28 +0200

    fix ManualBorderLAyout - JavaDoc

--- src/manualayoutsample/ManualBorderLayout.java
+++ src/manualayoutsample/ManualBorderLayout.java
@@ -36,38 +36,67 @@ import javax.swing.JFrame;
  * @version 11.10.2018
  */
 public class ManualBorderLayout extends JFrame {
-    
+    /**
+     * Button on the top of the App.
+     */
     private JButton topButton;
+    
+    /**
+     * Button on the right of the App.
+     */
     private JButton rightButton;
+    
+    /**
+     * Button on the bottom of the App.
+     */
     private JButton bottomButton;
+    
+    /**
+     * Button on the left of the App.
+     */
     private JButton leftButton;
+    
+    /**
+     * Button on the center of the App.
+     */
     private JButton centerButton;
+    
+    /**
+     * Constructor without parameters, set the 5 buttons and the layout.
+     */
     public ManualBorderLayout() {
         super();
         
+        // JFrame configuations
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setMinimumSize(new Dimension(500, 500));
         
+        // create and add the Top Button
         this.topButton = new JButton();
         this.topButton.setText("A");
         this.getContentPane().add(topButton, BorderLayout.PAGE_START);
         
+        // create and add the Right Button
         this.rightButton = new JButton();
         this.rightButton.setText("B");
         this.getContentPane().add(rightButton, BorderLayout.LINE_END);
         
+        // create and add the Bottom Button
         this.bottomButton = new JButton();
         this.bottomButton.setText("C");
         this.getContentPane().add(bottomButton, BorderLayout.PAGE_END);
         
+        // create and add the Left Button
         this.leftButton = new JButton();
         this.leftButton.setText("D");
         this.getContentPane().add(leftButton, BorderLayout.LINE_START);
         
+        // create and add the Center Button
         this.centerButton = new JButton();
         this.centerButton.setText("E");
         this.getContentPane().add(centerButton, BorderLayout.CENTER);
         
+        // recalcule size of the app.
         pack();
     }
     
