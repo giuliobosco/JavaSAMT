f02130885ae1db7c9cde6070cea92cc9ff6a2194
giuliobosco <giuliobva@gmail.com>
2018-11-22 12:25:15 +0100

    fix button package

--- src/button/Main.java
+++ src/button/Main.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package mybutton;
+package button;
 
 import javax.swing.JFrame;
 import java.awt.Graphics;
--- src/button/MyRoundButton.java
+++ src/button/MyRoundButton.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package mybutton;
+package button;
 
 import java.awt.Point;
 import java.awt.Color;
