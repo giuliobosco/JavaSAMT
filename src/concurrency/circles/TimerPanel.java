3eb4bd5742923b00fd51cc136929bd6fca42493d
giuliobosco <giuliobva@gmail.com>
2018-11-26 19:03:25 +0100

    add circles TimerPanel

---
+++ src/concurrency/circles/TimerPanel.java
@@ -0,0 +1,82 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package concurrency.circles;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * 
+ * @author giuliobosco
+ * @version 1.0
+ */
+public class TimerPanel extends JPanel implements MouseListener, TimerListener {
+
+
+    // -------------------------------------------------------------------------------------------------------- Costants
+    // ------------------------------------------------------------------------------------------------------ Attributes
+    // ----------------------------------------------------------------------------------------------- Getters & Setters
+    // ---------------------------------------------------------------------------------------------------- Constructors
+    // ---------------------------------------------------------------------------------------------------- Help Methods
+    // ------------------------------------------------------------------------------------------------- General Methods
+
+    @Override
+    public void timeElapsed(Timer source) {
+
+    }
+
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    // ----------------------------------------------------------------------------------------------- Static Components
+
+}
