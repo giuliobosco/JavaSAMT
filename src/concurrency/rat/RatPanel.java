b7b04bc9089eaba1f84a39bc80b4a2def921e7ef
giuliobosco <giuliobva@gmail.com>
2018-12-13 13:03:24 +0100

    add rat RatPanel

---
+++ src/concurrency/rat/RatPanel.java
@@ -0,0 +1,142 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package concurrency.rat;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Panel for the rats.
+ *
+ * @author giuliobosco
+ * @version 1.0
+ */
+public class RatPanel extends JPanel implements MouseListener, RatContainer {
+    // -------------------------------------------------------------------------------------------------------- Costants
+
+    /**
+     * Background dot size.
+     */
+    public final static int BG_DOT_SIZE = 2;
+
+    // ------------------------------------------------------------------------------------------------------ Attributes
+
+    /**
+     * List of rats.
+     */
+    private List<Rat> rats;
+
+    // ----------------------------------------------------------------------------------------------- Getters & Setters
+    // ---------------------------------------------------------------------------------------------------- Constructors
+
+    /**
+     * Constructor without parameters, initialize the rats list and add the mouse listener.
+     */
+    public RatPanel() {
+        this.rats = new ArrayList<>();
+        this.addMouseListener(this);
+    }
+
+    // ---------------------------------------------------------------------------------------------------- Help Methods
+
+    /**
+     * Add a rat to the rat list.
+     *
+     * @param rat Rat to add to the list.
+     */
+    public void addRat(Rat rat) {
+        this.rats.add(rat);
+    }
+
+    // ------------------------------------------------------------------------------------------------- General Methods
+
+    /**
+     * Rat moved, repaint the panel.
+     *
+     * @param source Moved rat.
+     */
+    @Override
+    public void ratMoved(Rat source) {
+        this.repaint();
+    }
+
+    /**
+     * Mouse clicked, create a new rat.
+     *
+     * @param e Mouse event.
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Rat rat = new Rat(this);
+        rat.start();
+        this.addRat(rat);
+        this.repaint();
+    }
+
+    /**
+     * Not used.
+     *
+     * @param e Mouse event.
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    /**
+     * Not used.
+     * @param e Mouse event.
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    /**
+     * Not used.
+     * @param e Mouse event.
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    /**
+     * Not used.
+     *
+     * @param e Mouse event.
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    // ----------------------------------------------------------------------------------------------- Static Components
+
+}
