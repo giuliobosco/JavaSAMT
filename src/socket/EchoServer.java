f93801dd6049502ba448dd78440e613d65d0781f
giuliobosco <giuliobva@gmail.com>
2019-01-20 18:37:04 +0100

    fix EchoServer - requests

--- src/socket/EchoServer.java
+++ src/socket/EchoServer.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
+
 package socket;
 
 import java.io.IOException;
@@ -29,14 +29,32 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
- * 
  * @author giuliobosco
  * @version 1.0
  */
 public class EchoServer {
     // ------------------------------------------------------------------------------------ Costants
+
+    /**
+     * Quit the connection command.
+     */
+    public static final String QUIT_COMMAND = "quit";
+
+    /**
+     * Get the actual time on the server command.
+     */
+    public static final String TIME_COMMAND = "time";
+
+    /**
+     * Shut down the server socket command.
+     */
+    public static final String SHUT_COMMAND = "shutdown";
+
     // ---------------------------------------------------------------------------------- Attributes
     // --------------------------------------------------------------------------- Getters & Setters
     // -------------------------------------------------------------------------------- Constructors
@@ -50,21 +68,48 @@ public class EchoServer {
 
             System.out.println("Server listening on port: 5555");
 
-            Socket client = server.accept();
+            boolean alive = true;
+            while (alive) {
+                Socket client = server.accept();
 
-            System.out.println("Connection from: " + client.getRemoteSocketAddress().toString());
+                System.out.println("Connection from: " + client.getRemoteSocketAddress().toString());
+                InputStream in = client.getInputStream();
+                OutputStream out = client.getOutputStream();
 
-            InputStream in = client.getInputStream();
-            OutputStream out = client.getOutputStream();
+                boolean connected = true;
+                int read = 0;
+                String command = "";
+                while ((read = in.read()) != -1 && connected) {
+                    if (read == 13 || read == 10) {
+                        command = "";
+                    } else {
+                        command += (char) read;
+                    }
 
-            int read = 0;
-            while ((read = in.read()) != -1) {
-                out.write(read);
+                    if (command.trim().equals(QUIT_COMMAND)) {
+                        connected = false;
+                    } else if (command.trim().equals(SHUT_COMMAND)) {
+                        connected = false;
+                        alive = false;
+                    } else if (command.trim().equals(TIME_COMMAND)) {
+                        Calendar cal = Calendar.getInstance();
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        String date = new Date().toString();//sdf.format(cal.getTime());
+                        for (int i = 0; i < date.length(); i++) {
+                            out.write(date.charAt(i));
+                        }
+                    } else {
+                        out.write(read);
+                    }
+                    System.out.println(command);
+                }
+                System.out.println("Connection close with :" + client.getRemoteSocketAddress().toString());
+
+                in.close();
+                out.close();
+                client.close();
             }
 
-            in.close();
-            out.close();
-            client.close();
         } catch (IOException ignored) {
 
         }
