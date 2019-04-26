c98aef43f97e0d45cf15f5e5bd0cffd9a09b7ae1
giuliobosco <giuliobva@gmail.com>
2019-04-26 14:59:11 +0200

    add help.BitsToByte - reverse operations
    Co-Authored-By: JariNaeser <jari.naeser@samtrevano.ch>

--- src/help/BitsToByte.java
+++ src/help/BitsToByte.java
@@ -5,7 +5,7 @@ import java.util.*;
  *
  * @author giuliobosco
  * @author jarinaeser
- * @version 1.0 (2019-04-26 - 2019-04-26)
+ * @version 1.1 (2019-04-26 - 2019-04-26)
  */
 public class BitsToByte {
 
@@ -19,7 +19,7 @@ public class BitsToByte {
         if (bits.length == 8) {
             int bitesSum = 0;
             for (int i = bits.length - 1; i >= 0; i--) {
-                bitesSum += Math.pow(2,(bits.length - i - 1)) * (bits[i]?1:0);
+                bitesSum += Math.pow(2, (bits.length - i - 1)) * (bits[i] ? 1 : 0);
             }
             return (byte) bitesSum;
         } else {
@@ -34,7 +34,7 @@ public class BitsToByte {
      * @param matrix Matrice da convertire in bytes.
      * @return Bytes della matrice.
      */
-    private static byte[] getBytes(boolean[][] matrix) {
+    public static byte[] getBytes(boolean[][] matrix) {
         if (matrix.length > 0) {
             int bytesCounter = 0;
             byte[] bytes = new byte[matrix.length * matrix[0].length / 8];
@@ -61,16 +61,74 @@ public class BitsToByte {
     }
 
     /**
+     * Converti un byte in 8 bits (array di booleani).
+     *
+     * @param b Byte da convertire in bits.
+     * @return Bits convertiti dal byte.
+     */
+    private static boolean[] getBits(byte b) {
+        int n = 0x000000FF & b;
+
+        boolean[] bits = new boolean[8];
+        int bitsCounter = bits.length - 1;
+
+        while (n > 0) {
+            bits[bitsCounter] = n % 2 == 1;
+            n /= 2;
+            bitsCounter--;
+        }
+
+        while (bitsCounter >= 0) {
+            bits[bitsCounter] = false;
+            bitsCounter--;
+        }
+
+        return bits;
+    }
+
+    /**
+     * Converti un array di byte in una matrice di booleani.
+     *
+     * @param bytes Bytes da convertire.
+     * @param rows Righe della matrice
+     * @param cols Colonne della matrice.
+     * @return
+     */
+    public static boolean[][] getMatrix(byte[] bytes, int rows, int cols) {
+        boolean[][] matrix = new boolean[rows][cols];
+
+        int x = 0, y = 0;
+
+        for (int k = 0; k < bytes.length; k++) {
+            boolean[] b = getBits(bytes[k]);
+            for (int i = 0; i < b.length; i++) {
+                matrix[x][y] = b[i];
+
+                y++;
+                if (y >= cols) {
+                    y = 0;
+                    x++;
+                    if (x > rows) {
+                        return matrix;
+                    }
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    /**
      * Metodo main della classe usato per testare i metodi.
      *
      * @param args Argomenti da linea di comando.
      */
     public static void main(String[] args) {
 
-		// test del metodo getByte(bits)
+        // test del metodo getByte(bits)
         // risultato apettato: -127
         boolean[] bits = {true, false, false, false, false, false, false, false};
-        System.out.println(new BitsToByte().getByte(bits));
+        System.out.println(getByte(bits));
 
         System.out.println();
 
@@ -80,9 +138,32 @@ public class BitsToByte {
                 {false, false, false, false, false, false, false, true, false, false, false, false},
                 {false, false, true, false, false, false, false, false, false, false, true, true}
         };
-        byte[] bytes = new BitsToByte().getBytes(matrix);
+        byte[] bytes = getBytes(matrix);
         for (byte b : bytes) {
             System.out.println(b);
+        }
+
+        System.out.println();
+
+        // test del metodo getBits(byte)
+        // risultato attesto true, false, false, false, false, false, false, false
+        bits = getBits((byte) -128);
+        for (int i = 0; i < bits.length; i++) {
+            System.out.println(bits[i]);
+        }
+
+        System.out.println();
+
+        // test del metodo getMatrix(bytes, rows, cols)
+        // risultato atteso:
+        // false false false false false false false true false false false false
+        // false false true false false false false false false false true true
+        matrix = getMatrix(bytes, 2, 12);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] +  " ");
+            }
+            System.out.println();
         }
     }
 }
